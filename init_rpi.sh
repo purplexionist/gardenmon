ca3d778169f4b8ee6fc46aa95ed5d7a2e1537ad5
Anthony Needles <needles.anthony@gmail.com>
2024-02-17 16:55:12 -0800

    python: add python/pip and requirements.txt

--- init_rpi.sh
+++ init_rpi.sh
@@ -13,4 +13,8 @@ sudo apt install vim -y
 # Enable serial port and enable interactive login.
 sudo raspi-config nonint do_serial 0
 
+# Install Python/Pip and installed required modules.
+sudo apt install python3 python3-pip -y
+pip install -r requirements.txt
+
 echo "You should reboot now..."
