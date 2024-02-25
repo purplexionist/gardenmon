bcd7017cdd66a1a26a628ff25595cf88f44f648b
Anthony Needles <needles.anthony@gmail.com>
2024-02-24 21:06:21 -0800

    setup: remove requirements.txt

--- init_rpi.sh
+++ init_rpi.sh
@@ -22,12 +22,9 @@ git config --global credential.helper store
 # Enable serial port and enable interactive login.
 sudo raspi-config nonint do_serial 0
 
-# Install smbus library for I2C in Python.
+# Install python3 and smbus library for I2C.
+sudo apt install python3 -y
 sudo apt install python3-smbus -y
-
-# Install Python/Pip and installed required modules.
-sudo apt install python3 python3-pip -y
-pip install -r requirements.txt
 
 # Enable I2C.
 sudo raspi-config nonint do_i2c 0
